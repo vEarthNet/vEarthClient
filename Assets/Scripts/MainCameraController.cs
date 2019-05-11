dc657eb5633c66fc15b5af9fdcae4d4c6d792e34
vEarthNet <admin@vearth.net>
2019-05-10 22:13:50 -0700

    Upgraded the camera controller to take direction forward/backward from the Y component on the touchpad, and rotation from the X component.

--- Assets/Scripts/MainCameraController.cs
+++ Assets/Scripts/MainCameraController.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class MainCameraController : MonoBehaviour
 {
 
     public float MouseSensitivity = 6;
     public float MoveSpeed = 5.0f;
+    public float RotateSpeed = 5.0f;
+    public float RotateDeadZone = 0.3f;
 
     public KeyCode MoveCamForward = KeyCode.W;
     public KeyCode MoveCamBackward = KeyCode.S;
@@ -15,10 +18,17 @@ public class MainCameraController : MonoBehaviour
     public KeyCode MoveCamUp = KeyCode.E;
     public KeyCode MoveCamDown = KeyCode.F;
 
+    private bool isTouching = false;
+
+    private Vector2 firstTouch;
+    private Vector2 lastTouch;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        firstTouch = new Vector2(0, 0);
+        lastTouch = new Vector2(0, 0);
     }
 
     // Update is called once per frame
@@ -49,17 +59,33 @@ public class MainCameraController : MonoBehaviour
     public void checkMove()
     {
         //Debug.Log("Checking move!!!!!!!!!! isTouching " + GvrControllerInput.IsTouching.ToString() + " click button " + GvrControllerInput.ClickButtonDown.ToString() 
-        //   + " touchpos " + GvrControllerInput.TouchPos.ToString());
+         //  + " touchpos " + GvrControllerInput.TouchPos.ToString());
 
         Vector3 camMove,camPos;
         
         //if ( Input.GetKey(MoveCamForward) || GvrControllerInput.IsTouching || GvrControllerInput.ClickButtonDown)
         if (GvrControllerInput.IsTouching)
         {
+            Vector2 touchPos = GvrControllerInput.TouchPos;
+            if (isTouching == false)
+            {
+                firstTouch = touchPos;
+                isTouching = true;
+            }
+
+            //First, convert the {0.0,1.0} scale to {-1.0,1.0}.
+            float forwardVel = 1.0f - (touchPos.y * 2.0f);
+            float turnVel = -1.0f + (touchPos.x * 2.0f);
+            
+
             GameObject player = Camera.main.transform.parent.gameObject;
-            camMove = Camera.main.transform.forward * MoveSpeed;
+            camMove = Camera.main.transform.forward * MoveSpeed * forwardVel;
             camPos = player.transform.position;
             player.transform.position = camPos + camMove;
+
+            if (Mathf.Abs(turnVel) > RotateDeadZone)
+                player.transform.Rotate(Vector3.up, turnVel * RotateSpeed );
+
         }/*
         else if (Input.GetKey(MoveCamBackward))
         {
